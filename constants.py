9afa9fdcb9d9eba796519e1c1229457b6c0bc1cc
Nacho Eigu <ignacio.eiguren@gmail.com>
2022-03-30 21:33:24 -0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+CONTRACT_ID_BSCAN = '0x7e624FA0E1c4AbFD309cC15719b7E2580887f570'
+CONTRACT_ID_ETHERSCAN = '0x83e6f1E41cdd28eAcEB20Cb649155049Fac3D5Aa'
+WEI = 1000000000000000000
+
+#These two variables you should change for the case of study
+CLOSE_INSCRIPTION_DATE = datetime(year = 2022, month = 4, day = 30, hour = 13, minute = 0, second = 0) #UTC time
+ADDRESSES = []
