8d6d3d3b7440d635e7ece21471333628b172dcf9
Nacho Eigu <ignacio.eiguren@gmail.com>
2022-04-28 20:47:19 -0300

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import requests
 import json
 from datetime import datetime, timedelta
-from constants import CONTRACT_ID_BSCAN, CONTRACT_ID_ETHERSCAN, WEI, CLOSE_INSCRIPTION_DATE
+from constants import STAKING_ADDRES_POLS_BSCAN, STAKING_ADDRES_POLS_ETHERSCAN, CONTRACT_ID_BSCAN, CONTRACT_ID_ETHERSCAN, WEI, CLOSE_INSCRIPTION_DATE
 import time
 import os
 
@@ -19,12 +19,12 @@ class Holder_Evaluator:
         #mode = True is Bscan
         #mode = False is Etherscan
         if mode == True:
-            url = f'https://api.bscscan.com/api?module=account&action=tokentx&contractaddress={CONTRACT_ID_BSCAN}&address={address}&page=1&offset=5&startblock=0&endblock=999999999&sort=asc&apikey={self.PRIVATE_KEY_BSCAN}'
+            url = f'https://api.bscscan.com/api?module=account&action=tokentx&contractaddress={CONTRACT_ID_BSCAN}&address={address}&page=1&offset=10000&startblock=0&endblock=999999999&sort=asc&apikey={self.PRIVATE_KEY_BSCAN}'
             data = requests.get(url)
             data = json.loads(data.content)
 
         else:
-            url = f"https://api.etherscan.io/api?module=account&action=tokentx&contractaddress={CONTRACT_ID_ETHERSCAN}&address={address}&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey={self.PRIVATE_KEY_ETHERSCAN}"
+            url = f"https://api.etherscan.io/api?module=account&action=tokentx&contractaddress={CONTRACT_ID_ETHERSCAN}&address={address}&page=1&offset=10000&startblock=0&endblock=27025780&sort=asc&apikey={self.PRIVATE_KEY_ETHERSCAN}"
             data = requests.get(url)
             data = json.loads(data.content)
 
@@ -36,7 +36,7 @@ class Holder_Evaluator:
         for transaction in reversed(data['result']):
             transaction_date = datetime.fromtimestamp(int(transaction['timeStamp']))
             if transaction_date > CLOSE_INSCRIPTION_DATE - timedelta(days = 7):
-                print("The transactions is after seven days of staking")
+                #The transactions is after seven days of staking
                 continue
 
             transaction_value = int(transaction['value'])/WEI
@@ -45,12 +45,62 @@ class Holder_Evaluator:
             #If the desired address withdraw tokens
             if sender == address:
                 output_transaction = transaction_value*-1
-                balance.append(output_transaction)
+                balance.append(float(output_transaction))
             else:
                 input_transaction = transaction_value
-                balance.append(input_transaction)
+                balance.append(float(input_transaction))
             
         return balance
+
+    def request_staking_contract(self, mode:bool, address):
+        #mode = True is Bscan
+        #mode = False is Etherscan
+
+        if mode == True:
+            url = f"https://api.bscscan.com/api?module=account&action=tokentx&contractaddress={CONTRACT_ID_BSCAN}&address={address}&page=1&offset=10000&startblock=0&endblock=999999999&sort=asc&apikey={self.PRIVATE_KEY_BSCAN}"
+            data = requests.get(url)
+            data = json.loads(data.content)
+
+            return data
+
+        else:
+            url = f"https://api.etherscan.io/api?module=account&action=tokentx&contractaddress={CONTRACT_ID_ETHERSCAN}&address={address}&page=1&offset=10000&startblock=0&endblock=999999999&sort=asc&apikey={self.PRIVATE_KEY_ETHERSCAN}"
+            data = requests.get(url)
+            data = json.loads(data.content)
+            content = []
+
+            return data
+
+    def balance_of_each_transaction(self, mode:bool, data):
+        if mode == True:
+            bsc_sent = []
+            bsc_received = []
+            for element in data['result']:
+                if datetime.fromtimestamp(int(element['timeStamp'])) > CLOSE_INSCRIPTION_DATE:
+                    continue
+                elif element['to'] == STAKING_ADDRES_POLS_BSCAN:
+                    bsc_sent.append(int(element['value'])/WEI)
+                elif element['from'] == STAKING_ADDRES_POLS_BSCAN:
+                    bsc_received.append(int(element['value'])/WEI)
+
+            total_staked = (sum(bsc_sent) + sum(bsc_received))
+            
+            return float(total_staked)
+
+        else:
+            eth_sent = []
+            eth_received = []
+            for element in data['result']:
+                if datetime.fromtimestamp(int(element['timeStamp'])) > CLOSE_INSCRIPTION_DATE:
+                    continue
+                elif element['to'] == STAKING_ADDRES_POLS_ETHERSCAN:
+                    eth_sent.append(int(element['value'])/WEI)
+                elif element['from'] == STAKING_ADDRES_POLS_ETHERSCAN:
+                    eth_received.append(int(element['value'])/WEI)
+
+            total_staked = sum(eth_sent) + sum(eth_received)
+
+            return float(total_staked)
 
     #This function makes everything happen so we analyze each winner address
     def analyzing_holders(self, list_addresses:list):
@@ -61,5 +111,15 @@ class Holder_Evaluator:
 
             etherscan_data = self.request_server(False, address)
             etherscan_balance = self.balance_of_each_address(etherscan_data, address)
-            
-            print(f"Address {address} = {(sum(etherscan_balance))} + {(sum(bscan_balance))}")
+            total_balance = sum(bscan_balance) + sum(etherscan_balance)
+
+            bscan_data = self.request_staking_contract(True, address)
+            bscan_balance = self.balance_of_each_transaction(True, bscan_data)
+
+            etherscan_data = self.request_staking_contract(False, address)
+            etherscan_balance = self.balance_of_each_transaction(False, etherscan_data)
+
+            total_balance = bscan_balance + etherscan_balance + total_balance
+
+            print(f"The address {address} held {str(total_balance)} before the inscription closes")
+                
